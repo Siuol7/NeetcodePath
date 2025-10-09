6b3a5f652ad463d55e670c9f4dd91ed856ab3801
Siuol <nguyencaotan03072000@gmail.com>
2025-10-10 02:43:33 +0300

    Update object version day 4

--- ArraysandHashing/GroupAnagrams/GroupAnagrams.ts
+++ ArraysandHashing/GroupAnagrams/GroupAnagrams.ts
@@ -22,5 +22,18 @@ function    GroupAnagram(strs : string[]) : string[][]
 
 function    GroupAnagram(strs : string[]) : string[][]
 {
-    const track = new Map(string, string[]);
+    const track = new Map<string, string[]>();
+
+    for (let s of strs)
+    {
+        const count = new Array(26).fill(0);
+        for (let c of s)
+            count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        let key = count.join(',');
+        if (!track.has(key))
+            track.set(key, []);
+        track.get(key)!.push(s);
+    }
+
+    return Array.from(track.values());
 }
