5cc40da93b7a84d6f70a2ec36831b79b7c6d07ee
Siuol <nguyencaotan03072000@gmail.com>
2025-10-13 01:38:42 +0300

    Update day 6

--- ArraysandHashing/ProductsofArrayExceptSelf/productExceptSelf.ts
+++ ArraysandHashing/ProductsofArrayExceptSelf/productExceptSelf.ts
@@ -1,16 +1,30 @@
 function    productExceptSelf(nums : number[]) : number[]
 {
-    let res = new Array(nums.length).fill(1);
+    let res = new Array(nums.length);
     let front = new Array(nums.length).fill(1);
     let back = new Array(nums.length).fill(1);
 
     for (let i = 0; i < nums.length; i++)
     {
-        if (i == 0) front[i] = nums[i];
+        if (i === 0) front[i] = nums[i];
         else
             front[i] = front[i - 1] * nums[i];
     }
-
-
+    for (let i = nums.length - 1; i > 0; i--)
+    {
+        if (i === nums.length - 1)
+        {
+            back[i] = nums[i];
+            res[i] = front[i - 1];
+        }
+        else
+        {
+            back[i] = back[i + 1] * nums[i];
+            res[i] = front[i - 1] * back[i + 1];
+        }
+    }
+    res[0] = back[1];
     return res;
 }
+
+console.log(productExceptSelf([-1,0,1,2,3]));
