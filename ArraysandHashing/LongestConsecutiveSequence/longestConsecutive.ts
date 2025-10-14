55dd99bf26b76827d41f41d5f1aa214f5e55f490
Siuol <nguyencaotan03072000@gmail.com>
2025-10-15 00:44:49 +0300

    Update day 8

--- ArraysandHashing/LongestConsecutiveSequence/longestConsecutive.ts
+++ ArraysandHashing/LongestConsecutiveSequence/longestConsecutive.ts
@@ -1,14 +1,20 @@
 function    longestConsecutive(nums : number[]) : number
 {
-    let     res : number;
+    let     res = 0;
     const   map = new Map();
 
     for (let n of nums)
     {
         if (!map.has(n))
-            map.set(n, map.get(n - 1) + map.get(n + 1) + 1);
-        
-    }
+        {
 
+            map.set(n, (map.get(n - 1) || 0) + (map.get(n + 1) || 0) + 1);
+        }
+        map.set(n - map.get(n - 1), map.get(n));
+        map.set(n + map.get(n + 1), map.get(n));
+        res = Math.max(map.get(n), res);        
+    }
     return res;
 }
+console.log(longestConsecutive([2,20,4,10,3,4,5]));
+console.log(longestConsecutive([0,3,2,5,4,6,1,1]));
