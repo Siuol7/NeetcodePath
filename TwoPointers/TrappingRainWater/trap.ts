f89f7d775b23e42e9a9efb3dae756456d6660343
Siuol <nguyencaotan03072000@gmail.com>
2025-10-20 21:30:22 +0300

    Update day 14

---
+++ TwoPointers/TrappingRainWater/trap.ts
@@ -0,0 +1,21 @@
+function    trap(height : number[]) : number
+{
+    let res = 0;
+    let Lcol = 0;
+    let Rcol = 0;
+    let left = 0;
+    let right = height.length - 1;
+
+    while (left < right)
+    {
+        if (height[left] < height[right])
+        {
+            // xu ly left
+        }
+        else
+        {
+            // xu ly right
+        }
+    }
+    return res;
+}
