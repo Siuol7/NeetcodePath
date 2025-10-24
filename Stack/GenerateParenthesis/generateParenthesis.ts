2cc3f4843b256ea20045a917aecb73bd5e164957
Siuol <nguyencaotan03072000@gmail.com>
2025-10-24 17:47:13 +0300

    Update day 18

---
+++ Stack/GenerateParenthesis/generateParenthesis.ts
@@ -0,0 +1,23 @@
+function    generateParenthesis(n : number) : string[]
+{
+    let res : string[][] = Array.from({length : n + 1}, () => []);
+    res[0] = [""];
+
+    for (let k = 0; k <= n; k++)
+        {
+        for (let i = 0; i < k; i++) 
+            {
+            for (const left of res[i]) 
+                {
+                for (const right of res[k - i - 1]) 
+                    {
+                        res[k].push('(' + left + ')' + right);
+                    }
+                }
+            }
+        }
+
+        return res[n];
+}
+
+console.log(generateParenthesis(3));
