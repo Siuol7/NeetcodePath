e043010da1157e18d743c20c6e7690d8fa14c740
Siuol <nguyencaotan03072000@gmail.com>
2025-10-27 22:13:11 +0200

    Update day 19

--- Stack/DailyTemperatures/dailyTemperatures.ts
+++ Stack/DailyTemperatures/dailyTemperatures.ts
@@ -6,7 +6,15 @@ function    dailyTemperatures(temperatures : number[]) : number[]
 
     for (let i = 0; i < l; i++)
     {
-
+        while (stack.length > 0 && temperatures[stack[stack.length - 1]] < temperatures[i])
+        {
+            let id = stack.pop()!;
+            res[id] = i - id;
+        }
+        stack.push(i);
     }
     return res;
 }
+
+console.log(dailyTemperatures([30,38,30,36,35,40,28])); 
+console.log(dailyTemperatures([22,21,20]));
